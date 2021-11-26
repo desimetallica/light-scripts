457fdf7bc30c33c2faf991017c1b370b45311c93
Davide Desirello <ddesi89@gmail.com>
2021-11-26 13:50:00 +0100

    brand new script
    custom borgbackup script with email and logs

---
+++ bash/musicBackup.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+#vars
+recipients="example@example.com"
+backupName="musicBackup"
+# Setting this, so the repo does not need to be given on the commandline:
+export BORG_REPO=somewhere
+
+# See the section "Passphrase notes" for more infos.
+export BORG_PASSPHRASE='example'
+export BORG_RELOCATED_REPO_ACCESS_IS_OK=yes
+
+# some helpers and error handling:
+info() { printf "\n%s %s\n\n" "$( date )" "$*" >&2; }
+trap 'echo $( date ) Backup interrupted >&2; exit 2' INT TERM
+
+logger -i "$backupName Starting backup"
+
+# Backup the most important directories into an archive named after
+# the machine this script is currently running on:
+
+backupOut=$(borg create                         \
+          --verbose                       \
+          --filter AME                    \
+          --list                          \
+          --stats                         \
+          --show-rc                       \
+          --compression lz4               \
+          --exclude-caches                \
+          --exclude '/tmp/*'              \
+          --exclude '/lost+found/*'       \
+                                          \
+          ::"{hostname}-{now}"            \
+          /whatToBackup 2>&1)
+
+backup_exit=$?
+
+logger -i "$backupName Pruning repository"
+
+# Use the `prune` subcommand to maintain 7 daily, 4 weekly and 6 monthly
+# archives of THIS machine. The '{hostname}-' prefix is very important to
+# limit prune's operation to this machine's archives and not apply to
+# other machines' archives also:
+
+pruneOut=$(borg prune                          \
+    --list                          \
+    --prefix "{hostname}-"          \
+    --show-rc                       \
+    --keep-last 4 2>&1)
+
+prune_exit=$?
+
+# use highest exit code as global exit code
+global_exit=$(( backup_exit > prune_exit ? backup_exit : prune_exit ))
+
+if [ ${global_exit} -eq 0 ]; then
+    logger -i "$backupName and Prune finished successfully"
+elif [ ${global_exit} -eq 1 ]; then
+     logger -i "[WARNING] $backupName and/or Prune finished with warnings"
+     logger -i "[WARNING] $backupName borg create comand error is: $backupOut"
+     logger -i "[WARNING] $backupName prune comand error is: $pruneOut"
+
+    /usr/sbin/sendmail "$recipients" <<- EOF
+subject:$backupName finished with warnings
+from:me@example.com
+to:$recipients
+
+This is an WARNING message.
+Something went wrong with the $backupName `date`
+
+Check the following waning messages:
+borg create cmd:
+$backupOut
+
+prune cmd:
+$pruneOut
+
+EOF
+
+else
+    logger -i "[ERROR] $backupName and/or Prune finished with errors"
+    logger -i "[ERROR] $backupName borg create comand error is: $backupOut"
+    logger -i "[ERROR] $backupName prune comand error is: $pruneOut"
+
+    /usr/sbin/sendmail "$recipients" <<- EOF
+subject:$backupName finished with errors
+from:me@example.com
+to:$recipients
+
+This is an ERROR message.
+Something went wrong with the $backupName `date`
+
+Check the following waning messages:
+borg create cmd:
+$backupOut
+
+prune cmd:
+$pruneOut
+EOF
+fi
+
+exit ${global_exit}
+
